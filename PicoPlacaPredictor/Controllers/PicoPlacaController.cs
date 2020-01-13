3c80e3e29ad96fd6d5c7d69c9d0705539db6c77e
felixminom <felix.minom@gmail.com>
2020-01-13 13:48:10 -0500

    REST API woking properly (test)

---
+++ PicoPlacaPredictor/Controllers/PicoPlacaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PicoPlacaPredictor.Controllers
+{
+    [RoutePrefix("api/PicoPlaca")]
+    public class PicoPlacaController : ApiController
+    {
+        [HttpPost]
+        [Route("CanRoad")]
+        public IHttpActionResult CanRoad()
+        {
+            return Ok(true);
+        }
+        
+    }
+}
