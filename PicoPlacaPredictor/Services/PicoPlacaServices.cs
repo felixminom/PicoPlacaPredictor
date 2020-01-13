2a3f7987a20c530707eff818fc0eae59aae78b2d
felixminom <felix.minom@gmail.com>
2020-01-13 15:26:24 -0500

    DayOfWeek and VerifyHour methods implemented.

---
+++ PicoPlacaPredictor/Services/PicoPlacaServices.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PicoPlacaPredictor.Models;
+
+namespace PicoPlacaPredictor.Services
+{
+    public class PicoPlacaServices
+    {
+        public static PicoPlacaResponseModel CanRoad(PicoPlacaModel picoPlacaAux)
+        {
+            PicoPlacaResponseModel response = new PicoPlacaResponseModel();
+
+            if(DayOfWeek(picoPlacaAux.Date)==0 || DayOfWeek(picoPlacaAux.Date) == 6)
+            {
+
+            }
+
+            return response;
+        }
+
+        private static int DayOfWeek(string dateAux)
+        {
+            string[] dateAsString = dateAux.Split('/');
+            int year = Convert.ToInt32(dateAsString[0]);
+            int month = Convert.ToInt32(dateAsString[1]);
+            int day = Convert.ToInt32(dateAsString[2]);
+
+            DateTime date = new DateTime(year, month, day);
+
+            int dayAsInt = (int)date.DayOfWeek;
+
+            return dayAsInt;
+
+        }
+
+
+        private static bool VerifyHour(string hour)
+        {
+            //we separate the hour recivied as string into hours and minutes and convert into integers
+            int hourAux = Convert.ToInt32(hour.Split(':')[0]);
+            int minutesAux = Convert.ToInt32(hour.Split(':')[1]);
+
+            //We validate the hours 
+       
+            if((hourAux>=7 && hourAux<=9) || (hourAux >=16 && hourAux <=19))
+            {
+                switch (hourAux)
+                {
+                    case 9:
+                        if (minutesAux <= 30)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    case 19:
+                        if (minutesAux <= 30)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+            
+        }
+    }
+}
