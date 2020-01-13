3900d5c218538a1b316ceade40dc47c8d78ab2bb
felixminom <felix.minom@gmail.com>
2020-01-13 14:49:59 -0500

    Models created.

---
+++ PicoPlacaPredictor/Models/PicoPlacaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PicoPlacaPredictor.Models
+{
+    public class PicoPlacaModel
+    {
+        public int PlateNumber { get; set;}
+        public string Date { get; set;}
+        public string Time { get; set; }
+    }
+
+    public class PicoPlacaResponseModel
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
