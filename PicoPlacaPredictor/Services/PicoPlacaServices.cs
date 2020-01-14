b1d1a33e672cdd92fe337bec5c985592773679e7
felixminom <felix.minom@gmail.com>
2020-01-14 12:17:10 -0500

    PlateNumberToDay function coded

--- PicoPlacaPredictor/Services/PicoPlacaServices.cs
+++ PicoPlacaPredictor/Services/PicoPlacaServices.cs
@@ -47,7 +47,6 @@ namespace PicoPlacaPredictor.Services
 
         }
 
-
         public bool VerifyHour(string hour)
         {
             //we separate the hour recivied as string into hours and minutes and convert into integers
@@ -87,11 +86,37 @@ namespace PicoPlacaPredictor.Services
             }
         }
 
-        /*public int PlateNumberToDay(int plateNumberAux)
+        public int PlateNumberToDay(int plateNumberAux)
         {
             //this function converts the plate number into a day of the week (from: monday to:friday)
+            int lastDigit = PlateNumberToDay(plateNumberAux);
 
-        }*/
+            switch (lastDigit)
+            {
+                case 1:
+                    return 1;
+                case 2:
+                    return 1;
+                case 3:
+                    return 2;
+                case 4:
+                    return 2;
+                case 5:
+                    return 3;
+                case 6:
+                    return 3;
+                case 7:
+                    return 4;
+                case 8:
+                    return 4;
+                case 9:
+                    return 5;
+                case 0:
+                    return 5;
+            }
+
+            return 0;
+        }
 
         public int LastPlateDigit(int plateNumberAux)
         {
